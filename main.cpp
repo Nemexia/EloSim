6cee868069beec92a808f91a3e8539e34196167d
Nemexia <nemexia.m@gmail.com>
2026-09-11 03:25:25 +0330

    Add Player

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+
+struct Player {
+  double skill;
+  double elo;
+};
 
 int main() {
+  std::vector<Player> players = {
+      {1700.0, 1500.0},
+      {1500.0, 1500.0},
+      {1800.0, 1500.0},
+  };
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
