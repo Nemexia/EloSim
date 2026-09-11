39cbbb2f4b8ddac62d35a0641fa8160198b5831b
Nemexia <nemexia.m@gmail.com>
2026-09-11 03:31:27 +0330

    Print players

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ int main() {
       {1500.0, 1500.0},
       {1800.0, 1500.0},
   };
-  std::cout << "Hello, World!" << std::endl;
+
+  for (auto const& player : players) {
+    std::cout << "Player skill: " << player.skill
+              << ", Player elo: " << player.elo << std::endl;
+  }
   return 0;
 }
